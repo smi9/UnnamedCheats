7609ccd592811534c731300b8ec013a8db0fdada
smi9 <147902076+smi9@users.noreply.github.com>
2025-04-13 20:48:25 -0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,18 @@
+if (getgenv().UC_LOADED) then
+	return;
+end;
+getgenv().UC_LOADED = true;
+
+if (identifyexecutor() == "Wave") then
+	getgenv().gethui = function()
+		return game:GetService("CoreGui");
+	end;	
+end;
+
+if (game.GameId == 6035872082) then
+	loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b6956594687d0095f5afdd3e2a2056ad.lua"))();
+elseif (game.GameId == 1008451066) then
+	loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/0326476cc171449b7563fb4e5eae0415.lua"))();
+elseif (game.PlaceId == 2317712696) then
+	loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/86943e213581a8598e8a38e5bba4c975.lua"))();
+end;
